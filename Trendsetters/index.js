57963ed86e389f35870290b72e146a5fc2d01e13
Cintia Arce <cintiaayelenarce@gmail.com>
2023-03-04 14:35:20 -0300

    trendsetters animations

---
+++ Trendsetters/index.js
@@ -0,0 +1,48 @@
+TweenMax.staggerFrom(".logo", 1.6, {
+    delay: 0.2,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".search", 1.6, {
+    delay: 0.2,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".img", 3, {
+    delay: 0.7,
+    opacity: 0,
+    y: 70,
+    ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".bottom-bar", 2, {
+    delay: 2,
+    opacity: 0,
+    y: -40,
+    ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".icn", 2, {
+    delay: 2.5,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".title span", 3, {
+    delay: 3,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".media ul li", 3, {
+    delay: 4.5,
+    opacity: 0,
+    y: 50,
+    ease: Expo.easeInOut
+})
