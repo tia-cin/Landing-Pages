9c9246dad81cea55dd4d5d3aba32a288f1dd672b
Cintia Arce <cintiaayelenarce@gmail.com>
2023-03-07 12:11:02 -0300

    project #6 animations

---
+++ Project #6/index.js
@@ -0,0 +1,26 @@
+let textWrapper = document.querySelector(".title");
+
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>")
+
+anime.timeline().add({
+    targets: ".title .letter",
+    translateY: [200, 0],
+    easing: "easeOutExpo",
+    duration: 1700,
+    opacity: 1,
+    delay: (el, i) => 400 + 70 * i
+})
+
+TweenMax.from(".scrolldown", 1.5, {
+    delay: 0.7,
+    opacity: 0,
+    y: 200,
+    ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".menu ul li", 1.5, {
+    delay: 1,
+    opacity: 0,
+    x: -30,
+    ease: Expo.easeInOut
+})
