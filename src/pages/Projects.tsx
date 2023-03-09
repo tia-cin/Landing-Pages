032ca5dfce64f50e70500ae2f603a7e0f88bb416
Cintia Arce <cintiaayelenarce@gmail.com>
2023-03-09 15:21:08 -0300

    templeates page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Projects: React.FC = () => {
   return (
-    <div id='projects' className='min-h-screen flex flex-col items-center'>
-        <h2 className='text-3xl font-bold mt-20'>Templates</h2>
-        <div>
-        </div>
+    <div
+      id="projects"
+      className="min-h-screen h-1000 flex flex-col items-center"
+    >
+      <h2 className="text-3xl font-bold mt-20">Templates</h2>
+      <div className="grid grid-cols-4 gap-40 mt-10">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((e) => (
+          <Link to={`/templates/project-${e}`}>
+            <section
+              key={e}
+              className="w-60 h-40 bg-white rounded flex flex-col items-center justify-center"
+            >
+              <h4 className="font-bold text-center">Project-{e}</h4>
+            </section>
+          </Link>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     colors: {
-      'white': '#e7e5e4',
-      'light-gray': '#d7d5d3',
-      'gray': '#b2b0ae',
-      'brown': '#a89283',
-      'black': '#252525'
+      white: "#e7e5e4",
+      "light-gray": "#d7d5d3",
+      gray: "#b2b0ae",
+      brown: "#a89283",
+      black: "#252525",
     },
     extend: {
+      height: {
+        300: "300px",
+        500: "500px",
+        1000: "1000px",
+      },
     },
   },
   plugins: [],
-}
+};
