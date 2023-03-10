f4700f778b639d22975743565d9821d19968b250
Cintia Arce <cintiaayelenarce@gmail.com>
2023-03-10 12:07:01 -0300

    project-3 animation

--- src/templates/Project3.tsx
+++ src/templates/Project3.tsx
@@ -3,18 +3,115 @@ import "../styles/project3.css";
 import shoe1 from "../assets/shoe1.png";
 import shoe2 from "../assets/shoe2.png";
 import {
-  BsSearch,
   BsFacebook,
   BsInstagram,
   BsTwitter,
   BsArrowLeft,
   BsArrowRight,
 } from "react-icons/bs";
-import { BiShoppingBag } from "react-icons/bi";
+import { BiShoppingBag, BiSearch } from "react-icons/bi";
 import { HiOutlineBars3 } from "react-icons/hi2";
-import { Expo, gsap } from "gsap";
+import { Expo, gsap, Power3 } from "gsap";
 
 const Project3 = () => {
+  React.useEffect(() => {
+    gsap.to(".first", {
+      duration: 1.5,
+      delay: 0.2,
+      left: -100,
+      ease: Expo.easeInOut,
+    });
+
+    gsap.to(".second", {
+      duration: 1.5,
+      delay: 0.4,
+      left: "-100%",
+      ease: Expo.easeInOut,
+    });
+
+    gsap.to(".third", {
+      duration: 1.5,
+      delay: 0.6,
+      left: "-100%",
+      ease: Expo.easeInOut,
+    });
+
+    // navbar
+    gsap.fromTo(
+      ".logo",
+      { duration: 1, delay: 1, opacity: 0 },
+      { duration: 1, delay: 1, opacity: 1, x: 20, ease: Expo.easeInOut }
+    );
+
+    gsap.fromTo(
+      ".menu",
+      { duration: 1, delay: 1.2, opacity: 0 },
+      { duration: 1, delay: 1.2, opacity: 1, x: 20, ease: Expo.easeInOut }
+    );
+
+    gsap.fromTo(
+      ".search",
+      { duration: 0.8, delay: 1.6, opacity: 0 },
+      { duration: 0.8, delay: 1.6, opacity: 1, x: 20, ease: Expo.easeInOut }
+    );
+
+    gsap.fromTo(
+      ".bag",
+      { duration: 1, delay: 1.6, opacity: 0 },
+      { duration: 1, delay: 1.6, opacity: 1, x: 20, ease: Expo.easeInOut }
+    );
+
+    // social media
+    gsap.fromTo(
+      ".media ul li",
+      { duration: 1, delay: 2, opacity: 0 },
+      { duration: 1, delay: 2, opacity: 1, x: 20, ease: Power3.easeInOut }
+    );
+
+    // product
+    gsap.fromTo(
+      ".size",
+      { duration: 1, delay: 1.8, opacity: 0 },
+      { duration: 1, delay: 1.8, opacity: 1, x: -20, ease: Expo.easeInOut }
+    );
+
+    gsap.fromTo(
+      ".size ul li",
+      { duration: 0.3, delay: 2, opacity: 0, y: 20 },
+      { duration: 0.3, delay: 2, opacity: 1, y: 0, ease: Power3.easeInOut }
+    );
+
+    gsap.fromTo(
+      ".product-title",
+      { duration: 3, delay: 2.2, opacity: 0, y: 50 },
+      { duration: 3, delay: 2.2, opacity: 1, y: 0, ease: Expo.easeInOut }
+    );
+
+    gsap.fromTo(
+      ".product-img",
+      { duration: 1, delay: 4.2, opacity: 0, y: -800 },
+      { duration: 1, delay: 4.2, opacity: 1, y: -300, ease: Expo.easeInOut }
+    );
+
+    // second product
+    gsap.fromTo(
+      ".bottom-right ul li:first-child",
+      { duration: 0.5, delay: 2.4, opacity: 0, x: -20 },
+      { duration: 0.5, delay: 2.4, opacity: 1, x: 0, ease: Expo.easeInOut }
+    );
+
+    gsap.fromTo(
+      ".bottom-right ul li:last-child",
+      { duration: 0.6, delay: 2.4, opacity: 0, x: -20 },
+      { duration: 0.6, delay: 2.4, opacity: 1, x: 0, ease: Expo.easeInOut }
+    );
+
+    gsap.fromTo(
+      ".bottom-img",
+      { duration: 1, delay: 2.4, opacity: 0, x: -20 },
+      { duration: 1, delay: 2.4, opacity: 1, x: 0, ease: Expo.easeInOut }
+    );
+  }, []);
   return (
     <div className="container">
       <div className="overlay first"></div>
@@ -29,7 +126,7 @@ const Project3 = () => {
           <HiOutlineBars3 />
         </div>
         <div className="search">
-          <BsSearch />
+          <BiSearch />
         </div>
         <div className="bag">
           <BiShoppingBag />
@@ -40,11 +137,6 @@ const Project3 = () => {
 
       <div className="product-text">
         <h1 className="product-title">puma.</h1>
-      </div>
-
-      <div className="product-desc">
-        <p>Elevate your training with our shoes</p>
-        <button>Shop now</button>
       </div>
 
       <div className="size">
