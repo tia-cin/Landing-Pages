77c1a522c3df874b2c5b4fb856cdeb1b78f3e4cb
Cintia Arce <cintiaayelenarce@gmail.com>
2023-03-10 14:22:49 -0300

    landing-4 setup

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Project1, Project2, Project3 } from "./templates";
+import { Project1, Project2, Project3, Project4 } from "./templates";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -17,6 +17,7 @@ root.render(
         <Route path="/templates/project-1" element={<Project1 />} />
         <Route path="/templates/project-2" element={<Project2 />} />
         <Route path="/templates/project-3" element={<Project3 />} />
+        <Route path="/templates/project-4" element={<Project4 />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
--- src/templates/index.ts
+++ src/templates/index.ts
@@ -1,5 +1,6 @@
 import Project1 from "./project-1/Project1";
 import Project2 from "./project-2/Project2";
 import Project3 from "./project-3/Project3";
+import Project4 from "./project-4/Project4";
 
-export { Project1, Project2, Project3 };
+export { Project1, Project2, Project3, Project4 };
