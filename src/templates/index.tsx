cf11c5b93b2258303746d16d69af2da923873ac7
Cintia Arce <cintiaayelenarce@gmail.com>
2023-03-10 12:35:20 -0300

    clear

--- src/templates/index.tsx
+++
@@ -1,6 +0,0 @@
-import Project1 from "./Project1";
-import Project2 from "./Project2";
-import Project3 from "./Project3";
-import Project4 from "./Project4";
-
-export { Project1, Project2, Project3, Project4 };
