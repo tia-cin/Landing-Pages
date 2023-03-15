4a1443062d2871b07d7ea5111621dcbd4503dae1
Cintia Arce <cintiaayelenarce@gmail.com>
2023-03-15 16:50:56 -0300

    project-12 animation

--- src/templates/project-12/Project12.tsx
+++ src/templates/project-12/Project12.tsx
@@ -5,29 +5,65 @@ import three from "../../assets/12-3.png";
 import four from "../../assets/12-4.png";
 import five from "../../assets/12-5.png";
 import "./project-12.css";
+import { gsap, Expo } from "gsap";
+import { ScrollTrigger } from "gsap/all";
 
-const Project12 = () => {
+gsap.registerPlugin(ScrollTrigger);
+
+const Project12: React.FC = () => {
+  const places = [one, two, three, four, five];
+  React.useEffect(() => {
+    let getRatio = (el: any) =>
+      window.innerHeight / (window.innerHeight + el.offsetHeight);
+
+    gsap.utils.toArray("section").forEach((section: any, i: number) => {
+      section.bg = section.querySelector(".bg");
+
+      section.bg.style.backgroundImage = "url(" + places[i] + ")";
+      console.log(i, section.bg.style.backgroundImage);
+
+      gsap.fromTo(
+        section.bg,
+        {
+          backgroundPosition: () =>
+            i ? `50% ${-window.innerHeight * getRatio(section)}px` : "50% 0px",
+        },
+        {
+          backgroundPosition: () =>
+            `50% ${window.innerHeight * (1 - getRatio(section))}px`,
+          ease: "none",
+          scrollTrigger: {
+            trigger: section,
+            start: () => (i ? "top bottom" : "top top"),
+            end: "bottom top",
+            scrub: true,
+            invalidateOnRefresh: true, // to make it responsive
+          },
+        }
+      );
+    });
+  }, []);
   return (
     <div className="project-12">
       <section>
         <div className="bg"></div>
-        <h1>Simple parallax sections</h1>
+        <h1>Consequat ullamco</h1>
       </section>
       <section>
         <div className="bg"></div>
-        <h1>Hey look, a title</h1>
+        <h1>eiusmod culpa amet</h1>
       </section>
       <section>
         <div className="bg"></div>
-        <h1>They just keep coming</h1>
+        <h1>cillum sunt aliquip</h1>
       </section>
       <section>
         <div className="bg"></div>
-        <h1>So smooth though</h1>
+        <h1>esse incididunt</h1>
       </section>
       <section>
         <div className="bg"></div>
-        <h1>Nice, right?</h1>
+        <h1>pariatur.</h1>
       </section>
     </div>
   );
