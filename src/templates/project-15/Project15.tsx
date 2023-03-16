a51c18add3a49900a78e60a64a3302dee7fec006
Cintia Arce <cintiaayelenarce@gmail.com>
2023-03-16 15:40:10 -0300

    project-15 new setup

--- src/templates/project-15/Project15.tsx
+++ src/templates/project-15/Project15.tsx
@@ -1,91 +1,26 @@
 import React from "react";
+import top from "../../assets/15-1.png";
+import bottom from "../../assets/15-2.png";
 
 const Project15 = () => {
   return (
     <div>
-      <h1>February 2022</h1>
-      <p>Holidays and Daily Observances in the United States</p>
-      <div className="wrap">
-        <ul>
-          <li>
-            <p>1</p>
-            <span>Dark Chocolate Day</span>
-          </li>
-          <li>
-            <p>2</p>
-            <span>Groundhog Day</span>
-          </li>
-          <li>
-            <p>3</p>
-            <span>Carrot Cake Day</span>
-          </li>
-          <li>
-            <p>4</p>
-            <span>Wear Red Day</span>
-          </li>
-          <li>
-            <p>5</p>
-            <span>Weatherperson's Day</span>
-          </li>
-          <li>
-            <p>6</p>
-            <span>Chopsticks Day</span>
-          </li>
-          <li>
-            <p>7</p> <span>Periodic Table Day</span>
-          </li>
-          <li>
-            <p>8</p> <span>Kite Flying Day</span>
-          </li>
-          <li>
-            <p>9</p> <span>Pizza Day</span>
-          </li>
-          <li>
-            <p>10</p>
-            <span>Umbrella Day</span>
-          </li>
-          <li>
-            <p>11</p>
-            <span>Inventor's Day</span>
-          </li>
-          <li>
-            <p>12</p>
-            <span>Global Movie Day</span>
-          </li>
-          <li>
-            <p>13</p>
-            <span>Tortellini Day</span>
-          </li>
-          <li>
-            <p>14</p>
-            <span>Valentine's Day</span>
-          </li>
-          <li>
-            <p>15</p>
-            <span>Gumdrop Day</span>
-          </li>
-          <li>
-            <p>16</p>
-            <span>Do a Grouch a Favor Day</span>
-          </li>
-          <li>
-            <p>17</p>
-            <span>Cabbage Day</span>
-          </li>
-          <li>
-            <p>18</p>
-            <span>Battery Day</span>
-          </li>
-          <li>
-            <p>19</p>
-            <span>Chocolate Mint Day</span>
-          </li>
-          <li>
-            <p>20</p>
-            <span>Love Your Pet Day</span>
-          </li>
-        </ul>
-      </div>
+      <h1 className="header-section">Scroll to see what it's made of</h1>
+
+      <section className="glock">
+        <img src={top} className="barrel piece" alt="barrel" />
+        <div>
+          <img src={top} className="piece" alt="spring" />
+          <img src={bottom} className="piece" alt="spring" />
+        </div>
+        <div>
+          <img src={top} className="piece" alt="spring" />
+          <img src={bottom} className="piece" alt="spring" />
+        </div>
+        <img src={bottom} className="base piece" alt="base" />
+      </section>
+
+      <h1 className="header-section">Boom.</h1>
     </div>
   );
 };
