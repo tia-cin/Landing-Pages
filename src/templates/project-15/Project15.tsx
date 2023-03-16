c64bcc89351e53c9c6f441f22cb1ebfe509f3c51
Cintia Arce <cintiaayelenarce@gmail.com>
2023-03-16 16:33:22 -0300

    project-15 animation

--- src/templates/project-15/Project15.tsx
+++ src/templates/project-15/Project15.tsx
@@ -1,23 +1,43 @@
 import React from "react";
 import top from "../../assets/15-1.png";
 import bottom from "../../assets/15-2.png";
+import { gsap } from "gsap";
+import "./project-15.css";
 
-const Project15 = () => {
+const Project15: React.FC = () => {
+  React.useEffect(() => {
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: ".glock",
+          start: "top top",
+          end: "bottom bottom",
+          markers: true,
+          scrub: true,
+        },
+      })
+      .to(".three .head", { yPercent: -60 })
+      .to(".three .base", { yPercent: 65 })
+      .to(".two .head", { yPercent: -30 })
+      .to(".two .base", { yPercent: 35 });
+  }, []);
   return (
-    <div>
-      <h1 className="header-section">Scroll to see what it's made of</h1>
+    <div className="project-15">
+      <h1 className="header-section">Scroll to see what it's inside</h1>
 
       <section className="glock">
-        <img src={top} className="barrel piece" alt="barrel" />
-        <div>
-          <img src={top} className="piece" alt="spring" />
-          <img src={bottom} className="piece" alt="spring" />
+        <div className="one">
+          <img src={top} className="head piece" alt="barrel" />
+          <img src={bottom} className="base piece" alt="base" />
         </div>
-        <div>
-          <img src={top} className="piece" alt="spring" />
-          <img src={bottom} className="piece" alt="spring" />
+        <div className="two">
+          <img src={top} className="head piece" alt="spring" />
+          <img src={bottom} className="base piece" alt="spring" />
         </div>
-        <img src={bottom} className="base piece" alt="base" />
+        <div className="three">
+          <img src={top} className="head piece" alt="spring" />
+          <img src={bottom} className="base piece" alt="spring" />
+        </div>
       </section>
 
       <h1 className="header-section">Boom.</h1>
