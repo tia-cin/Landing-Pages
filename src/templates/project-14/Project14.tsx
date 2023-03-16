85697906d2640c6a395e1e7331776b52fb2c3d9d
Cintia Arce <cintiaayelenarce@gmail.com>
2023-03-16 15:05:09 -0300

    project-14 reset

--- src/templates/project-14/Project14.tsx
+++ src/templates/project-14/Project14.tsx
@@ -7,82 +7,95 @@ gsap.registerPlugin(ScrollTrigger);
 
 const Project14: React.FC = () => {
   const colors = [
-    "#243119",
+    "#ebefe3",
     "#c9f2c7",
-    "#269460",
     "#aceca1",
     "#96be8c",
-    "#269460",
-    "#aceca1",
-    "#96be8c",
+    "#629460",
+    "#243119",
   ];
   React.useEffect(() => {
-    gsap
-      .timeline({
-        scrollTrigger: {
-          trigger: ".grid-container",
-          start: "top top",
-          end: "bottom bottom",
-          scrub: true,
-          pin: ".grid",
-          anticipatePin: 1,
-        },
-      })
-      .set(".gridBlock:not(.centerBlock)", { autoAlpha: 0 })
-      .to(".gridBlock:not(.centerBlock)", { duration: 0.1, autoAlpha: 1 })
-      .from(".gridLayer", { scale: 3.3333, ease: "none" });
-    gsap.set(".gridBlock", {
-      backgroundColor: (i) => colors[i],
-    });
+    const container = document.querySelector(".slides"),
+      slides = gsap.utils.toArray(".slide"),
+      getRatio = (el: any) =>
+        window.innerHeight / (window.innerHeight + el.offsetHeight);
 
-    const bigImg = document.querySelector(".centerBlock");
-    bigImg?.addEventListener("load", () => {
-      gsap.to(".centerPiece .gridBlock", { autoAlpha: 1, duration: 0.5 });
+    slides.forEach((slide: any, i) => {
+      const bg = slide.querySelector(".background"),
+        content = slide.querySelector(".content"),
+        tl = gsap.timeline({
+          scrollTrigger: {
+            trigger: slide,
+            start: () => (i ? "top bottom" : "top top"),
+            end: "bottom top",
+            scrub: true,
+            invalidateOnRefresh: true,
+          },
+        });
+
+      tl.fromTo(
+        bg,
+        { y: () => (i ? -window.innerHeight * getRatio(slide) : 0) },
+        { y: () => window.innerHeight * (1 - getRatio(slide)), ease: "none" }
+      );
+      tl.fromTo(
+        content,
+        { y: () => (i ? window.innerHeight * -getRatio(slide) * 2 : 0) },
+        { y: () => window.innerHeight * getRatio(slide) * 2, ease: "none" },
+        0
+      );
     });
   }, []);
   return (
     <div className="project-14">
-      <h1 className="header-section">
-        Scroll down to see a photo gallery being revealed
-      </h1>
-
-      <div className="grid-container">
-        <div className="grid">
-          <div className="gridLayer">
-            <div className="gridBlock"></div>
-          </div>
-          <div className="gridLayer">
-            <div className="gridBlock"></div>
-          </div>
-          <div className="gridLayer">
-            <div className="gridBlock"></div>
-          </div>
-          <div className="gridLayer centerPiece">
-            <div className="gridBlock centerBlock"></div>
-          </div>
-          <div className="gridLayer">
-            <div className="gridBlock">
-              <a href="https://greensock.com" target="_blank"></a>
-            </div>
-          </div>
-          <div className="gridLayer">
-            <div className="gridBlock"></div>
-          </div>
-          <div className="gridLayer">
-            <div className="gridBlock"></div>
-          </div>
-          <div className="gridLayer">
-            <div className="gridBlock"></div>
-          </div>
-          <div className="gridLayer">
-            <div className="gridBlock"></div>
-          </div>
-        </div>
-      </div>
-
-      <h1 className="header-section" style={{ marginTop: 0 }}>
-        Final
-      </h1>
+      <section className="ends">Start</section>
+      <section className="slides">
+        <ul className="list">
+          <li className="slide">
+            <div
+              className="background"
+              style={{ backgroundColor: colors[0] }}
+            ></div>
+            <div className="content"> 1</div>
+          </li>
+          <li className="slide">
+            <div
+              className="background"
+              style={{ backgroundColor: colors[1] }}
+            ></div>
+            <div className="content"> 2</div>
+          </li>
+          <li className="slide">
+            <div
+              className="background"
+              style={{ backgroundColor: colors[2] }}
+            ></div>
+            <div className="content"> 3</div>
+          </li>
+          <li className="slide">
+            <div
+              className="background"
+              style={{ backgroundColor: colors[3] }}
+            ></div>
+            <div className="content"> 4</div>
+          </li>
+          <li className="slide">
+            <div
+              className="background"
+              style={{ backgroundColor: colors[4] }}
+            ></div>
+            <div className="content"> 5</div>
+          </li>
+          <li className="slide">
+            <div
+              className="background"
+              style={{ backgroundColor: colors[5] }}
+            ></div>
+            <div className="content"> 6</div>
+          </li>
+        </ul>
+      </section>
+      <section className="ends">End</section>
     </div>
   );
 };
