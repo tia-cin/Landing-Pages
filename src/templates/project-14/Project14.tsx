2cfa6508d8b8b57b0c892aedb440971c44c159fc
Cintia Arce <cintiaayelenarce@gmail.com>
2023-03-16 11:28:55 -0300

    project-14 animation setup

--- src/templates/project-14/Project14.tsx
+++ src/templates/project-14/Project14.tsx
@@ -1,7 +1,45 @@
 import React from "react";
 import "./project-14.css";
+import { Expo, gsap } from "gsap";
+import { ScrollTrigger } from "gsap/all";
 
-const Project14 = () => {
+gsap.registerPlugin(ScrollTrigger);
+
+const Project14: React.FC = () => {
+  const colors = [
+    "#243119",
+    "#c9f2c7",
+    "#269460",
+    "#aceca1",
+    "#96be8c",
+    "#269460",
+    "#aceca1",
+    "#96be8c",
+  ];
+  React.useEffect(() => {
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: ".grid-container",
+          start: "top top",
+          end: "bottom bottom",
+          scrub: true,
+          pin: ".grid",
+          anticipatePin: 1,
+        },
+      })
+      .set(".gridBlock:not(.centerBlock)", { autoAlpha: 0 })
+      .to(".gridBlock:not(.centerBlock)", { duration: 0.1, autoAlpha: 1 })
+      .from(".gridLayer", { scale: 3.3333, ease: "none" });
+    gsap.set(".gridBlock", {
+      backgroundColor: (i) => colors[i],
+    });
+
+    const bigImg = document.querySelector(".centerBlock");
+    bigImg?.addEventListener("load", () => {
+      gsap.to(".centerPiece .gridBlock", { autoAlpha: 1, duration: 0.5 });
+    });
+  }, []);
   return (
     <div className="project-14">
       <h1 className="header-section">
